00df57781b64153be1c02417c907c336f7d97830
powerboat9-school <33042784+powerboat9-school@users.noreply.github.com>
2019-01-30 20:13:03 -0500

    Did some work

--- src/com/powerboat9/instrument/FolderFSManager.java
+++ src/com/powerboat9/instrument/FolderFSManager.java
@@ -1,9 +1,41 @@
-public class FolderFSManager implements IFSManager {
-    
+package com.powerboat9.instrument;
 
-    public FolderFSManager(File fIn) throws IOException {
-        if (!fIn.isDirrectory())
+import java.nio.*;
+import java.io.*;
+
+public class FolderFSManager implements IFSManager {
+    private Path dir;
+    private WatchService watcher;
+    private WatchKey watchKey;
+    
+    private Object modLock;
+
+    private HashMap<HashObject, Path> files = new HashMap<>();
+    
+    public FolderFSManager(Path fIn) throws IOException {
+        fIn = fIn.getAbsoluteFile();
+        if (!fIn.isDirectory()) throw new IOException("Not a directory");
+        if (!fIn.canRead()) throw new IOException("Cannot read directory");
+        dir = fIn;
+        watcher = dir.getFileSystem().newWatcherServive();
+        watchKey = watcher.register(dir, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+    }
+    
+    public static void rebuild() {
+        synchronized (modLock) {
+            
 
     @Override
     public Reader getFile(byte[] in) {
-        if (in.length)
+        synchronized (modLock) {
+        }
+    }
+    
+    @Override
+    public void update() {
+        synchronized (modLock) {
+            List<WatchEvent<?>> es = watchKey.pollEvents();
+            watchKey.reset();
+            for (WatchEvent<?> e : es) {
+                
+}
